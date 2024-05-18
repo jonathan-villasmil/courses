5b73519772030dfd98eec8d4c1869cf09c6c99a2
jonathan <jonathanvillasmildeabreu@gmail.com>
2024-05-18 02:46:43 +0200

    Items per page categoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,13 +10,13 @@ use Inertia\Response;
 
 class CategoryController extends Controller
 {
+    const NUMBER_OF_ITEMS_PER_PAGE = 25;
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        define('NUMBER_OF_ITEMS_PER_PAGE',25);
-        $categories = Category::paginate(NUMBER_OF_ITEMS_PER_PAGE);
+        $categories = Category::paginate(self::NUMBER_OF_ITEMS_PER_PAGE);
         return inertia('Categories/Index',['categories' => $categories]);
     }
 
